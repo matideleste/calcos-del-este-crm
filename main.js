480de0a2ec2d4fd9babf2ee8726f687c2c966446
matideleste <maticalcosdeleste@gmail.com>
2026-09-06 14:25:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+console.log('SUPABASE URL:', supabaseUrl)
+console.log('SUPABASE KEY:', supabaseAnonKey)
 const supabase = supabaseUrl && supabaseAnonKey ? createClient(supabaseUrl, supabaseAnonKey) : null
 
 const state={session:null, section:'Inicio', entries:[], reminders:[], due:[]}
